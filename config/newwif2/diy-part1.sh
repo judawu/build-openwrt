ccc9d5fb4b8934feca928677e6bb606b28e14151
judawu <59474654+judawu@users.noreply.github.com>
2024-07-30 21:06:41 +0800

    Update diy-part1.sh

--- config/newwif2/diy-part1.sh
+++ config/newwif2/diy-part1.sh
@@ -2,4 +2,3 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-
