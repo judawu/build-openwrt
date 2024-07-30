2a47f41f236f969baae858988cd6c5354f388a03
judawu <59474654+judawu@users.noreply.github.com>
2024-07-30 21:47:54 +0800

    Update diy-part2.sh

--- config/newwif2/diy-part2.sh
+++ config/newwif2/diy-part2.sh
@@ -1,2 +1,2 @@
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.10.1）
-sed -i 's/192.168.1.1/192.168.10.1/g'openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
